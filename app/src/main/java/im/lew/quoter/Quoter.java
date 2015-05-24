352c30b6ae351482eb3d440da0d3caea6c506521
Lewis Lebentz <lewislebentz@users.noreply.github.com>
2015-05-24 22:52:54 +0100

    Updated background and added Settings page

--- app/src/main/java/im/lew/quoter/Quoter.java
+++ app/src/main/java/im/lew/quoter/Quoter.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.content.Intent;
+
 import java.util.ArrayList;
 
 
@@ -21,17 +23,23 @@ public class Quoter extends ActionBarActivity {
         RelativeLayout touch = (RelativeLayout) findViewById(R.id.touch);
         final TextView quoteText = (TextView) findViewById(R.id.quote);
         final TextView personText = (TextView) findViewById(R.id.person);
-        final ArrayList<Quote> quoteList = new ArrayList<Quote>();
-        Quote quote4 = new Quote("You're more of a fun vampire. You don't suck blood, you just suck.", "Troy Barnes");
-                quoteList.add(quote4);
-        Quote quote1 = new Quote("Cool Beans", "Rod Kimble");
+        final ArrayList<Quote> quoteList = new ArrayList<>();
+
+        Quote quote1 = new Quote("You're more of a fun vampire. You don't suck blood, you just suck.", "Troy Barnes");
         quoteList.add(quote1);
-        Quote quote2 = new Quote("How can mirrors be real if our eyes aren't real", "Jaden Smith");
+
+        Quote quote2 = new Quote("Cool Beans", "Rod Kimble");
         quoteList.add(quote2);
-        Quote quote3 = new Quote("That's like me blaming owls for how bad I suck at analogies.", "Britta Perry");
-                quoteList.add(quote3);
+
+        Quote quote3 = new Quote("How can mirrors be real if our eyes aren't real", "Jaden Smith");
+        quoteList.add(quote3);
+
+        Quote quote4 = new Quote("That's like me blaming owls for how bad I suck at analogies.", "Britta Perry");
+        quoteList.add(quote4);
+
         Quote quote5 = new Quote("I was gonna be the first person in my family to graduate from community college. Everyone else graduated from normal college", "Troy Barnes");
         quoteList.add(quote5);
+
         touch.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -41,7 +49,7 @@ public class Quoter extends ActionBarActivity {
                     quoteText.setText(q.getQuote());
                     personText.setText(q.getPerson());
                     count = count + 1;
-                } else{
+                } else {
                     count = 0;
                 }
             }
@@ -59,13 +67,17 @@ public class Quoter extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(Quoter.this, Settings.class);
+            startActivity(intent);
+            return true;
+        }
+
+        else if (id == R.id.exit_the_app){
+
+            finish();
             return true;
         }
 
